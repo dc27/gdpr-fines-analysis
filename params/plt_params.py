e0bfe241a9ce69820b1b9206fb8b499b58cb38e0
David Currie <dcurrie27@gmail.com>
2023-11-13 22:42:49 +0000

    a lot of time on not a lot of plotting changes

---
+++ params/plt_params.py
@@ -0,0 +1,19 @@
+import matplotlib as mpl
+
+# oft-used style
+mpl.rcParams.update(
+    {
+        "font.size": 12,
+        "grid.color": "darkgrey",
+        "grid.linestyle": ":",
+        "axes.grid": True,
+        "axes.facecolor": "white",
+        "axes.spines.left": False,
+        "axes.spines.bottom": False,
+        "axes.spines.top": False,
+        "axes.spines.right": False,
+        "ytick.left": False,
+        "xtick.bottom": False,
+        "figure.figsize": (8, 5)
+    }
+)
