9d2cc9c3541cb065cae8f5bfe11d404522ccc096
David Currie <dcurrie27@gmail.com>
2023-12-06 19:30:52 +0000

    adds interpretation and figure captions. re-renders

--- params/plt_params.py
+++ params/plt_params.py
@@ -3,19 +3,19 @@ import matplotlib as mpl
 # oft-used style
 mpl.rcParams.update(
     {
-        "font.size": 12,
-        "grid.color": "darkgrey",
-        "grid.linestyle": ":",
-        "axes.grid": True,
-        "axes.facecolor": "white",
-        "axes.spines.left": False,
+        "font.size"         : 10,
+        "grid.color"        : "darkgrey",
+        "grid.linestyle"    : ":",
+        "axes.grid"         : True,
+        "axes.facecolor"    : "white",
+        "axes.spines.left"  : False,
         "axes.spines.bottom": False,
-        "axes.spines.top": False,
-        "axes.spines.right": False,
-        "ytick.left": False,
-        "xtick.bottom": False,
-        "figure.facecolor": "white",
-        "figure.figsize": (7.4, 5),
-        "figure.autolayout": True
+        "axes.spines.top"   : False,
+        "axes.spines.right" : False,
+        "ytick.left"        : False,
+        "xtick.bottom"      : False,
+        "figure.facecolor"  : "white",
+        "figure.figsize"    : (7.3, 4.5),
+        "figure.autolayout" : True
     }
 )
